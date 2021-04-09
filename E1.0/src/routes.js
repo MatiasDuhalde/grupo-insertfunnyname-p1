6561489e12e89af9c5f4673b1fc33dac656d5ccc
Matias Duhalde <matias.duhalde@uc.cl>
2021-04-09 18:01:43 -0400

    fix: add missing routes file

--- E1.0/src/routes.js
+++ E1.0/src/routes.js
@@ -2,10 +2,12 @@ const KoaRouter = require('koa-router');
 
 const hello = require('./routes/hello');
 const index = require('./routes/index');
+const profile = require('./routes/profile');
 
 const router = new KoaRouter();
 
 router.use('/', index.routes());
 router.use('/hello', hello.routes());
+router.use('/profile', profile.routes());
 
 module.exports = router;

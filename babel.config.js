96b2f041cc4557e611d4d05a85dc9f6a147f9c59
Matias Duhalde <matias.duhalde@uc.cl>
2021-04-25 17:15:36 -0400

    refactor: change babel.config.js file indentation

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,26 @@
 module.exports = function babelConfig(api) {
   const babelEnv = api.env();
   const presets = [
-    ['@babel/env', {
-      targets: {
-        ie: '9',
-        browsers: ['>1%', 'last 3 versions'],
+    [
+      '@babel/env',
+      {
+        targets: {
+          ie: '9',
+          browsers: ['>1%', 'last 3 versions'],
+        },
+        modules: babelEnv === 'test' ? 'auto' : false,
+        useBuiltIns: 'usage',
+        corejs: 3,
       },
-      modules: babelEnv === 'test' ? 'auto' : false,
-      useBuiltIns: 'usage',
-      corejs: 3,
-    }],
-    ['@babel/preset-react', {
-      development: babelEnv === 'development',
-    }],
+    ],
+    [
+      '@babel/preset-react',
+      {
+        development: babelEnv === 'development',
+      },
+    ],
   ];
 
-  const plugins = [
-    'react-hot-loader/babel',
-  ];
+  const plugins = ['react-hot-loader/babel'];
   return { presets, plugins };
 };

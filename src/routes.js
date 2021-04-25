766c3b3411516d3a917c552d6e944e6d85c7959c
Matias Duhalde <matias.duhalde@uc.cl>
2021-04-24 21:19:28 -0400

    build: remove hello example files

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 const KoaRouter = require('koa-router');
 
-const hello = require('./routes/hello');
 const index = require('./routes/index');
 const profile = require('./routes/profile');
 const posts = require('./routes/posts');
@@ -8,7 +7,6 @@ const posts = require('./routes/posts');
 const router = new KoaRouter();
 
 router.use('/', index.routes());
-router.use('/hello', hello.routes());
 router.use('/profile', profile.routes());
 router.use('/posts', posts.routes());
 

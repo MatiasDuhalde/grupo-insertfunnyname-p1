ae06c71a2feb1d62bd7951e8520b7b571cec1069
Matias Duhalde <matias.duhalde@uc.cl>
2021-04-27 01:11:37 -0400

    fix: undo deleted view html

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,11 +6,16 @@ const { renderUserPage } = require('./utils/render');
 const router = new KoaRouter();
 
 router.param('userId', validateIntParam);
+router.param('page', validateIntParam);
 
 router.get('users.show', '/:userId', loadCurrentUser, renderUserPage);
 
 router.get('users.show.posts', '/:userId/posts', loadCurrentUser, renderUserPage);
 
-router.get('users.show.liked', '/:userId/liked', loadCurrentUser, renderUserPage);
+router.get('users.show.posts', '/:userId/posts/:page', loadCurrentUser, renderUserPage);
+
+router.get('users.show.posts', '/:userId/liked', loadCurrentUser, renderUserPage);
+
+router.get('users.show.liked', '/:userId/liked/:page', loadCurrentUser, renderUserPage);
 
 module.exports = router;

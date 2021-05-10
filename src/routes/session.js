fa960dc1e8027608bb48d2ef0b3e245cf4cc6196
Matias Duhalde <matias.duhalde@uc.cl>
2021-05-09 23:08:51 -0400

    feat: add working login

--- src/routes/session.js
+++ src/routes/session.js
@@ -4,23 +4,45 @@ const { renderLoginPage, renderSignupPage } = require('./utils/render');
 
 const router = new KoaRouter();
 
-router.get('session.login-form', '/login', loadCurrentUser, renderLoginPage);
+router.get(
+  'session.login-form',
+  '/login',
+  loadCurrentUser,
+  async (ctx, next) => {
+    if (ctx.state.currentUser) {
+      return ctx.redirect(ctx.router.url('index.home'));
+    }
+    return next();
+  },
+  renderLoginPage,
+);
 
 router.post('session.login', '/login', async (ctx, next) => {
   const { email, password } = ctx.request.body;
-  const user = await getUserByEmail(email);
-
-  if (user) {
+  const user = await getUserByEmail(ctx, email);
+  if (user && (await user.validatePassword(password))) {
+    // TODO: Encrypt user.id in cookie (use crypto module)
     ctx.session.currentUserId = user.id;
     ctx.redirect(ctx.router.url('index.home'));
   } else {
+    // TODO: Add error message
     ctx.redirect(ctx.router.url('session.login-form'));
   }
-
   return next();
 });
 
-router.get('session.signup-form', '/signup', loadCurrentUser, renderSignupPage);
+router.get(
+  'session.signup-form',
+  '/signup',
+  loadCurrentUser,
+  async (ctx, next) => {
+    if (ctx.state.currentUser) {
+      return ctx.redirect(ctx.router.url('index.home'));
+    }
+    return next();
+  },
+  renderSignupPage,
+);
 
 router.post('session.signup', '/signup', async (ctx, next) => {
   const { firstName, lastName, email, password } = ctx.request.body;

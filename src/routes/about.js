92f32a92adb525afcea51402fcb5561fbe7b2337
Matias Duhalde <matias.duhalde@uc.cl>
2021-05-17 04:51:04 -0400

    fix: delete about routes file

--- src/routes/about.js
+++
@@ -1,9 +0,0 @@
-const KoaRouter = require('koa-router');
-
-const router = new KoaRouter();
-
-router.get('index.home', '/', (ctx) => {
-  ctx.redirect(ctx.router.url('posts.index'));
-});
-
-module.exports = router;

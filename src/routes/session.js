41f38689eed2eb9effa2357f14675d66d359e3ba
Matias Duhalde <matias.duhalde@uc.cl>
2021-05-18 19:36:10 -0400

    feat: add session error handling with flash message

--- src/routes/session.js
+++ src/routes/session.js
@@ -11,11 +11,10 @@ router.post('session.login', '/login', excludeLogin, async (ctx, next) => {
   const { email, password } = ctx.request.body;
   const user = await getUserByEmail(ctx, email);
   if (user && (await user.validatePassword(password))) {
-    // TODO: Encrypt user.id in cookie (use crypto module)
     ctx.session.currentUserId = user.id;
     ctx.redirect(ctx.router.url('index.home'));
   } else {
-    // TODO: Add error message
+    ctx.flashMessage.danger = { login: 'Incorrect email or password' };
     ctx.redirect(ctx.router.url('session.login-form'));
   }
   return next();
@@ -30,30 +29,28 @@ router.post('session.signup', '/signup', excludeLogin, async (ctx) => {
     email,
     password,
   } = ctx.request.body;
-  const hashedPassword = await ctx.orm.User.generateHash(password);
   let user;
   try {
-    if (password.length < 6) {
-      const validationError = new ctx.orm.Sequelize.ValidationError();
-      const item = new ctx.orm.Sequelize.ValidationErrorItem(
-        'Password is too short',
-        'Validation error',
-        'password',
-        password,
-      );
-      validationError.errors.push(item);
-      throw validationError;
-    }
+    const hashedPassword = await ctx.orm.User.generateHash(password);
     user = await ctx.orm.User.create({
       firstName,
       lastName,
       email,
       hashedPassword,
-      avatarLink: 'https://png.pngtree.com/png-vector/20191026/ourlarge/pngtree-avatar-vector-icon-white-background-png-image_1870181.jpg',
+      avatarLink:
+        'https://png.pngtree.com/png-vector/20191026/ourlarge/pngtree-avatar-vector-icon-white-background-png-image_1870181.jpg',
       coverLink: 'https://llandscapes-10674.kxcdn.com/wp-content/uploads/2019/07/lighting.jpg.webp',
     });
-  } catch (validationError) {
-    ctx.flashMessage.error = validationError.errors;
+  } catch (err) {
+    const messages = {};
+    if (err instanceof ctx.orm.Sequelize.ValidationError) {
+      err.errors.forEach((errorItem) => {
+        messages[errorItem.path] = errorItem.message;
+      });
+    } else {
+      messages.password = err.message;
+    }
+    ctx.flashMessage.danger = messages;
     return ctx.redirect('back');
   }
   ctx.session.currentUserId = user.id;

1877108b39b35385fbab9506794e700cf47797ae
Matias Duhalde <matias.duhalde@uc.cl>
2021-05-23 22:34:12 -0400

    feat: add posts model tests

--- src/models/__tests__/post.test.js
+++ src/models/__tests__/post.test.js
@@ -1,30 +1,44 @@
-// const orm = require('..');
+const orm = require('..');
 
-// describe('post model', () => {
-//   beforeAll(async () => {
-//     await orm.sequelize.sync({ force: true });
-//   });
+describe('user model', () => {
+  const sampleUserData = {
+    firstName: 'John',
+    lastName: 'Web',
+    email: 'user@example.org',
+    password: '12345678',
+    avatarLink: 'https://cdn.fakercloud.com/avatars/tgerken_128.jpg',
+    coverLink: 'https://picsum.photos/seed/0.8774069009477856/1000/500',
+  };
 
-//   afterAll(async () => {
-//     await orm.sequelize.close();
-//   });
-  
+  const mocked = {};
 
-//   describe('dummyPost', () => {
-//     const postData = {
-//       imageLink: '',
-//       body: 'Insert Funny Name test',
-// 			userId: 1,
-//     };
+  beforeAll(async () => {
+    sampleUserData.hashedPassword = await orm.User.generateHash('12345678');
+    await orm.sequelize.sync({ force: true });
+    mocked.user = await orm.User.create(sampleUserData);
+  });
 
-//     beforeAll(async () => {
-//       await orm.Post.create(postData);
-//     });
+  afterAll(async () => {
+    await orm.sequelize.close();
+  });
 
-//     test('', async () => {
-//       const { imageLink, body, userId }  = postData;
-//       const post = await orm.Post.findOne{ where: { body } });
-//       expect().toThrowError('Password must be at least 6 characters');
-//     });
-//   });
-// });
+  describe('dummyPost', () => {
+    const samplePostData = {
+      imageLink: 'https://picsum.photos/seed/0.8774069009477856/1000/500',
+      body: 'Insert Funny Name test',
+    };
+
+    beforeAll(async () => {
+      samplePostData.userId = mocked.user.id;
+    });
+
+    test('userId must not be null', async () => {
+      const badPostData = { ...samplePostData, userId: undefined };
+      await expect(() => orm.Post.create(badPostData)).rejects.toThrowError();
+    });
+
+    test('create post', async () => {
+      await expect(orm.Post.create(samplePostData)).resolves.toBeInstanceOf(orm.Post);
+    });
+  });
+});
